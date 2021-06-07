5f1c8e3194623a0155071f336e4cfc82879e0112
Kofeinich <59574446+Kofeinich@users.noreply.github.com>
2021-06-07 13:28:44 +0300

    fix: changes

--- src/ru/billing/tunes/util/ItemDAO.java
+++ src/ru/billing/tunes/util/ItemDAO.java
@@ -16,17 +16,17 @@ public class ItemDAO
     public MusicItem searchById(Long id) throws SQLException {
         MusicItem result = null;
         Statement stmt = null;
-        String sql = String.format("SELECT * FROM APP.ITEM WHERE ITEM_ID = %d", id);
+        var sql = String.format("SELECT * FROM APP.ITEM WHERE ITEM_ID = %d", id);
         try
         {
             stmt = m_conn.createStatement();
-            ResultSet reSet = stmt.executeQuery(sql);
+            var reSet = stmt.executeQuery(sql);
             if (reSet.next()) {
-                String title = reSet.getString("TITLE");
-                String artist = reSet.getString("ARTIST");
-                Date releaseDate = reSet.getDate("RELEASEDATE");
-                BigDecimal listPrice = reSet.getBigDecimal("LISTPRICE");
-                BigDecimal price = reSet.getBigDecimal("PRICE");
+                var title = reSet.getString("TITLE");
+                var artist = reSet.getString("ARTIST");
+                var releaseDate = reSet.getDate("RELEASEDATE");
+                var listPrice = reSet.getBigDecimal("LISTPRICE");
+                var price = reSet.getBigDecimal("PRICE");
                 result = new MusicItem(id, title, artist, releaseDate, listPrice, price);
             }
         }
@@ -39,19 +39,20 @@ public class ItemDAO
     public Collection<MusicItem> searchByKeyword(String keyword)
             throws SQLException
     {
-        Collection<MusicItem> result = new ArrayList<MusicItem>();
-        String wildcarded = "%" + keyword + "%";
-        String sql = "SELECT * FROM GUEST.ITEM WHERE TITLE LIKE ?";
-        PreparedStatement pstmt = m_conn.prepareStatement(sql);
-        ResultSet reSet = pstmt.executeQuery();
+        var result = new ArrayList<MusicItem>();
+        var wildcarded = "%" + " " + keyword + " " + "%";
+        var sql = "SELECT * FROM APP.ITEM WHERE TITLE LIKE ?";
+        var pstmt = m_conn.prepareStatement(sql);
+        pstmt.setString(1, wildcarded);
+        var reSet = pstmt.executeQuery();
         while (reSet.next()) {
             long id = reSet.getLong("ITEM_ID");
-            String title = reSet.getString("TITLE");
-            String artist = reSet.getString("ARTIST");
-            Date releaseDate = reSet.getDate("RELEASEDATE");
-            BigDecimal listPrice = reSet.getBigDecimal("LISTPRICE");
-            BigDecimal price = reSet.getBigDecimal("PRICE");
-            MusicItem ItemValue = new MusicItem(id, title, artist, releaseDate, listPrice, price);
+            var title = reSet.getString("TITLE");
+            var artist = reSet.getString("ARTIST");
+            var releaseDate = reSet.getDate("RELEASEDATE");
+            var listPrice = reSet.getBigDecimal("LISTPRICE");
+            var price = reSet.getBigDecimal("PRICE");
+            var ItemValue = new MusicItem(id, title, artist, releaseDate, listPrice, price);
             result.add(ItemValue);
         }
         return result;
@@ -60,9 +61,9 @@ public class ItemDAO
     public void create(MusicItem item)
             throws SQLException
     {
-        java.sql.Date releaseDate = new java.sql.Date(item.getReleaseDate().getTime());
+        var releaseDate = new java.sql.Date(item.getReleaseDate().getTime());
         String sql = "INSERT INTO APP.ITEM (TITLE, ARTIST, RELEASEDATE, LISTPRICE, PRICE, VERSION)" + "VALUES (?, ?, ?, ?, ?, ?)";
-        PreparedStatement stmt = m_conn.prepareStatement(sql);
+        var stmt = m_conn.prepareStatement(sql);
         stmt.setString(1, item.getTitle());
         stmt.setString(2, item.getArtist());
         stmt.setDate(3, releaseDate);
--- src/ru/billing/tunes/util/ItemDAOMain.java
+++ src/ru/billing/tunes/util/ItemDAOMain.java
@@ -11,10 +11,10 @@ public class ItemDAOMain {
         Connection conn = null;
         Class.forName("org.apache.derby.jdbc.ClientDriver");
         conn = DriverManager.getConnection("jdbc:derby://localhost:1527/MyDbTest;create=true;", "Guest", "1234");
-        DatabaseMetaData dbmd = conn.getMetaData();
+        var dbmd = conn.getMetaData();
         System.out.println(dbmd.getDriverName());
         System.out.println(dbmd.getUserName());
-        ItemDAO itemDAO = new ItemDAO(conn);
+        var itemDAO = new ItemDAO(conn);
         musicItem = itemDAO.searchById(1L);
         if (musicItem != null) {
             System.out.println(musicItem.toString());
@@ -28,25 +28,16 @@ public class ItemDAOMain {
         } else {
             System.out.println("not found");
         }
-/*
-        System.out.println("result of searching \"of\":");
-        Collection<MusicItem> result2 = itemDAO.searchByKeyword("of");
-        if (result2 != null) {
-            for (MusicItem item: result2){
+
+
+        System.out.println("result of searching: ");
+        var result1 = itemDAO.searchByKeyword("of");
+        if (result1 != null) {
+            for (MusicItem item: result1){
                 System.out.println(item.toString());
             }
-        } else {
-            System.out.println("not found");
         }
 
-        System.out.println("result of searching \"Gay\":");
-        result2 = itemDAO.searchByKeyword("Gay");
-        if (result2 != null) {
-            for (MusicItem item: result2){
-                System.out.println(item.toString());
-            }
-        }
-        */
         musicItem = new MusicItem(22L, "Greatest", "Guns N Roses", new Date(), new BigDecimal("13.6"), new BigDecimal("19.54"));
         itemDAO.create(musicItem);
         conn.close();
